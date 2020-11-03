0b6760c6e86275540382e835dedc4885ea98c67a
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-03 13:59:43 -0600

    WIP

--- src/constants/fonts/primary.ts
+++ src/constants/fonts/primary.ts
@@ -1,9 +1,7 @@
-const FONTS = {
+export const primaryFonts = {
   headerFont:
     "'nocturno', Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif",
   baseFont:
     "'larssiet', 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
   monoFont: "'apercu mono', 'Lucida Console', Monaco, monospace",
 };
-
-export default FONTS;
--- src/constants/fonts/secondary.ts
+++ src/constants/fonts/secondary.ts
@@ -1,9 +1,10 @@
-const FONTS = {
+/**
+ * TODO-theming: Update font values once provided
+ */
+export const secondaryFonts = {
   headerFont:
     "'nocturno', Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif",
   baseFont:
     "'larssiet', 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
   monoFont: "'apercu mono', 'Lucida Console', Monaco, monospace",
 };
-
-export default FONTS;
